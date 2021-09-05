6d5b36bdf46dec5687df288df689a622f8d70fc2
VJS <minsung_96@naver.com>
2021-09-05 23:56:40 +0900

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { AutoComplete } from '@react-google-maps/api'
+import { AppBar, Toolbar, InputBase, Box } from '@material-ui/core' 
+import { mergeClasses } from '@material-ui/styles'
+
+import SearchIcon from '@material-ui/icons/Search'
+
+import useStyles from './styles'
 
 const Header = () => {
+
+    const classes = useStyles()
+
     return (
-        <div>
-            Header!
-        </div>
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Your Travel Navigator
+                </Typography>
+                <Box diaplay="flex">
+                    <Typography variant="h6" className={classes.title}>
+                        Find New Places Around You.
+                    </Typography>
+                    <AutoComplete>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput}}/>
+                        </div>
+                    </AutoComplete>
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
 
